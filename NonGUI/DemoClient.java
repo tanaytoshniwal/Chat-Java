ab39f2e782624c4d31453cc0fe002262a0badfb7
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-12-07 02:17:12 +0530

    some changes

--- NonGUI/DemoClient.java
+++ NonGUI/DemoClient.java
@@ -3,8 +3,11 @@ import java.net.*;
 
 public class DemoClient {
 	static Socket clientSocket=null;
+	static DemoClient obj;
+	static Receive2 receiver;
+	static Send2 sender;
 	class Send2 extends Thread{
-		public void run(){
+		synchronized public void run(){
 			try{
 				OutputStream os=clientSocket.getOutputStream();
 				DataOutputStream dos=new DataOutputStream(os);
@@ -19,11 +22,11 @@ public class DemoClient {
 				}
 				dos.close();
 			}
-			catch(IOException e){System.out.println("exception occured");}
+			catch(IOException e){System.exit(0);}
 		}
 	}
 	class Receive2 extends Thread{
-		public void run(){
+		synchronized public void run(){
 			try{
 				BufferedReader br=null;
 				try{
@@ -41,15 +44,15 @@ public class DemoClient {
 				}
 				br.close();
 			}
-			catch(IOException e){System.out.println("exception caught");}
+			catch(IOException e){System.exit(0);}
 		}
 	}
 	public static void main(String[] args) throws IOException{
 		clientSocket=new Socket(InetAddress.getLocalHost(),99);
-		DemoClient obj=new DemoClient();
+		obj=new DemoClient();
 		
-		Receive2 receiver=obj.new Receive2();
-		Send2 sender=obj.new Send2();
+		receiver=obj.new Receive2();
+		sender=obj.new Send2();
 		receiver.start();
 		sender.start();
 	}
